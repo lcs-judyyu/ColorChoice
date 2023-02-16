ff8f06f301fc3853845f025af1651659c30034be
Judy Yu <judy.yuqiuyi@gmail.com>
2023-02-15 23:20:54 -0500

    Initial Commit

---
+++ ColorChoice/ColorChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChoiceApp.swift
+//  ColorChoice
+//
+//  Created by Judy YU on 2023-02-15.
+//
+
+import SwiftUI
+
+@main
+struct ColorChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
